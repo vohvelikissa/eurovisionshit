e8dc8b393eb1b85cbc1b5b5f45875f0b7213f2d4
Pessi <--add>
2024-05-12 00:42:22 +0300

    added dynamic year

--- eurovisionshit.sh
+++ eurovisionshit.sh
@@ -1,1 +1,1 @@
-w3m $(w3m "duckduckgo.com/?q=eurovision+finalist+order+2024" | grep "eurovision.tv") | grep "-" | grep "|" | grep -v "CEST" | nvim
+w3m $(w3m "duckduckgo.com/?q=eurovision+finalist+order+$(date \"+%Y\")" | grep "eurovision.tv") | grep "-" | grep "|" | grep -v "CEST" | nvim
